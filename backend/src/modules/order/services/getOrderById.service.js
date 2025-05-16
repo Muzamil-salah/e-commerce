c85f8e17150fab4ae1bad7a3d9d51ed94e33b263
Muzamil-salah <hana2021mohsen@gmail.com>
2025-05-16 17:38:31 +0300

    admin updated

--- backend/src/modules/order/services/getOrderById.service.js
+++ backend/src/modules/order/services/getOrderById.service.js
@@ -11,9 +11,9 @@ const getByOrderId=async(req , res , next)=>{
             return res.status(404).json({status:'fail' , message:'order not found'})
         }
         
-        if(order.user._id.toString() !== req.user._id.toString()){
-            return res.status(401).json({status:'fail' , message:'not authorized'})
-        }
+        // if(order.user._id.toString() !== req.user._id.toString()){
+        //     return res.status(401).json({status:'fail' , message:'not authorized'})
+        // }
     
         return res.status(200).json({status:'success' , order})
     } catch (error) {
